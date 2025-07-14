97b4dda7b5e004f3ca1e3d6e9a9e25c34eed09fa
raimundo-vicuna <raivicuna@gmail.com>
2025-07-13 21:51:48 -0400

    final

--- practicatest.js
+++ practicatest.js
@@ -1,4 +1,5 @@
 (() => {
+    console.log('© 2025 rai. Uso y distribución permitidos bajo licencia estándar.')
     const cards = document.querySelectorAll('.card[id^="quest_"]');
     cards.forEach((card) => {
         const correctOption = parseInt(card.querySelector('.option_correct')?.textContent.trim());
@@ -6,7 +7,7 @@
         const listItems = Array.from(card.querySelectorAll('.list-group-item'));
         const position = orderAnswer.indexOf(correctOption);
         listItems[position].classList.add('active')
-        // console.log(`Correct Option: ${correctOption}\n Order Answer: ${orderAnswer}\n List Items: ${listItems}\n Position: ${position}`)
+        console.log(`Correct Option: ${correctOption}\n Order Answer: ${orderAnswer}\n List Items: ${listItems}\n Position: ${position}`)
         
     });
 
